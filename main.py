7efdbc82bf8cf680f80631d7ce0719f633165b0b
Francesc0rtu <francescortu@live.it>
2023-01-09 15:18:50 +0100

    update

--- main.py
+++ main.py
@@ -1,30 +1,98 @@
-from flask import Flask, make_response, request, render_template
+import logging
+import os.path
+
+from flask import Flask, render_template, request, redirect, flash, send_from_directory
+from werkzeug.utils import secure_filename
+
+# [logging config
+logging.basicConfig(format='%(asctime)s:%(levelname)s:%(filename)s:%(funcName)s:%(message)s',
+                    datefmt='%Y-%m-%d %H:%M:%S',
+                    level=logging.INFO)
+# logging config]
+
 
 app = Flask(__name__)
+app.secret_key = "somesecretkey"
 
-def transform(text_file_contents):
-    return text_file_contents.replace("=", ",")
+app.config['ALLOWED_EXTENSIONS'] = ['.jpg', '.png', '.csv', '.zip']
+app.config['MAX_CONTENT_LENGTH'] = 1 * 1024 * 1024
+
+UPLOAD_FOLDER = os.path.join(os.getcwd(), 'DATA/input_data')
+DOWNLOAD_FOLDER  = os.path.join(os.getcwd(), 'DATA/download')
 
 
-@app.route('/')
-def form():
-    return render_template("index.html")
+# http://localhost:5000
+@app.route('/', methods=['GET'])
+def index():
+    logging.info('Showing index page')
+    return render_template('upload.html')
 
-@app.route('/transform', methods=["POST"])
-def transform_view():
-    request_file = request.files['data_file']
-    data_type = request.form["data_type"]
-    if not request_file:
-        return "No file"
 
-    file_contents = request_file.stream.read().decode("utf-8")
+@app.route('/', methods=['POST'])
+def upload_files():
+    """Upload a file."""
+    logging.info('Starting file upload')
 
-    if data_type == "json":
-        result = transform(file_contents)
-    elif data_type == "csv":
-        result  =transform(file_contents)
+    if 'file' not in request.files:
+        flash('No file part')
+        return redirect(request.url)
 
-    print(data_type)
-    response = make_response(result)
-    response.headers["Content-Disposition"] = "attachment; filename=result.csv"
-    return response
+    file = request.files['file']
+    metadata  = request.files['metadata']
+    # obtaining the name of the destination file
+    filename = file.filename
+    metadata_name  = metadata.filename
+    if filename == '':
+        logging.info('Invalid file')
+        flash('No file selected for uploading')
+        return redirect(request.url)
+    else:
+        logging.info('Selected file is= [%s]', filename)
+        file_ext = os.path.splitext(filename)[1]
+        if file_ext in app.config['ALLOWED_EXTENSIONS']:
+            secure_fname = secure_filename(filename)
+            file.save(os.path.join(UPLOAD_FOLDER, secure_fname))
+            logging.info('Upload is successful')
+            flash('File uploaded successfully')
+            return redirect('/')
+        else:
+            logging.info('Invalid file extension')
+            flash('Not allowed file type')
+            return redirect(request.url)
+
+
+@app.route('/download/<path:filename>', methods=['GET'])
+def download(filename):
+    """Download a file."""
+    logging.info('Downloading file= [%s]', filename)
+    logging.info(app.root_path)
+    full_path = os.path.join(app.root_path, DOWNLOAD_FOLDER)
+    logging.info(full_path)
+    return send_from_directory(full_path, filename, as_attachment=True)
+
+
+# http://localhost:5000/files
+@app.route('/files', methods=['GET'])
+def list_files():
+    """Endpoint to list files."""
+    logging.info('Listing already uploaded files from the upload folder.')
+    upf = []
+    for filename in os.listdir(DOWNLOAD_FOLDER):
+        path = os.path.join(DOWNLOAD_FOLDER, filename)
+        if os.path.isfile(path):
+            upf.append(filename)
+
+    # return jsonify(uploaded_files)
+    return render_template('list.html', files=upf)
+
+
+def check_upload_dir():
+    if not os.path.exists(UPLOAD_FOLDER):
+        os.makedirs(UPLOAD_FOLDER, exist_ok=True)
+
+
+if __name__ == '__main__':
+    check_upload_dir()
+    # Development only: run "python app.py" and open http://localhost:5000
+    server_port = os.environ.get('PORT', '5000')
+    app.run(debug=False, port=server_port, host='0.0.0.0')
---
+++ src/database.py
@@ -0,0 +1,91 @@
+import pymongo
+from pymongo.server_api import ServerApi
+import os
+import base64
+from getpass import getpass
+import gridfs
+from dotenv import load_dotenv
+from src.util import bcolors
+
+
+load_dotenv("config/.env")
+class Database():
+    def __init__(self):
+        self.db = self.connect_to_database()
+        self.InputData = gridfs.GridFS(self.db, 'InputData')
+        self.OutputData = gridfs.GridFS(self.db, 'OutputData')
+        self.input_path = "DATA/input/"
+        self.output_path = "DATA/output/"
+
+    def connect_to_database(self):
+        '''Connect to the database, if the user has not saved the credential in env variable, ask for them.
+        The mongodb databse is auth also with IP.
+        '''
+        #print("To connect to the database, please enter the following information:")
+        # auto_login = input("Do you want connect with saved credential? (y/n): ")
+        # if auto_login == "y":
+        #     username = os.getenv("MONGO_USER")
+        #     password = os.getenv("MONGO_PASS")
+        #     mongo_host = os.getenv("MONGO_HOST")
+        # else:
+        #     mongo_host = input("Name of the mongo project: ")
+        #     username = input("Username: ") 
+        #     password = getpass()
+
+        username = os.getenv("MONGO_USER")
+        password = os.getenv("MONGO_PASS")
+        mongo_host = os.getenv("MONGO_HOST")
+            
+        #connect to the database
+        client = pymongo.MongoClient(f"mongodb+srv://{username}:{password}@{mongo_host}", server_api=ServerApi('1'))
+        db = client['SDP']
+
+        print(bcolors.OKGREEN + "Connected to the database" + bcolors.ENDC)
+        return db
+
+    def load_input_data(self, filename, username):
+        path = "DATA/input/" + filename
+
+        with open(path, "rb") as file:
+            encoded = base64.b64encode(file.read())
+        self.InputData.put(encoded, filename=filename, username=username)
+    
+    def load_output_data(self, filename, base_dataset_filename, username):
+        path = "DATA/output/" + filename
+        with open(path, "rb") as file:
+            encoded = base64.b64encode(file.read())
+        self.OutputData.put(encoded, filename=filename, username=username, base_dataset=base_dataset_filename)
+
+    def get_input_data(self, filename, username):
+        path = "DATA/input/" + filename
+
+        #if the file is already in the local machine do not retrive
+        if os.path.exists(path):
+            return 
+        file = self.InputData.find_one(filter={"filename": filename, "username": username})
+        with open(path, 'wb') as f:  
+            f.write(base64.b64decode(file.read()))
+        
+    def get_output_data(self, filename, username):
+        path = "DATA/output/" + filename
+
+        #if the file is already in the local machine do not retrive
+        if os.path.exists(path):
+            return 
+        file = self.OutputData.find_one(filter={"filename": filename, "username": username})
+        with open(path, 'wb') as f:  
+            f.write(base64.b64decode(file.read()))
+
+
+
+
+
+
+    
+
+
+
+
+
+
+
